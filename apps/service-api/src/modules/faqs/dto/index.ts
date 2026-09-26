31ac05a3485a8bad0f305fd79f7f3fa49fff8892
jyk0128 <wlsyong90@gmail.com>
2026-09-26 12:01:20 +0900

    refactor(faqs): reorder exports for clarity

--- apps/service-api/src/modules/faqs/dto/index.ts
+++ apps/service-api/src/modules/faqs/dto/index.ts
@@ -1,8 +1,8 @@
 export { CreateFaqRequestDto } from './create-faq.request.dto';
-export { FaqDetailResponseDto } from './faq-detail.response.dto';
 export { FaqCursorResponseDto } from './faq-cursor.response.dto';
+export { FaqDetailResponseDto } from './faq-detail.response.dto';
 export { FaqItemDto } from './faq-item.dto';
+export { FaqListResponseDto } from './faq-list.response.dto';
 export { GetFaqsRequestDto } from './get-faqs.request.dto';
 export { GetPublicFaqsRequestDto } from './get-public-faqs.request.dto';
-export { FaqListResponseDto } from './faq-list.response.dto';
 export { UpdateFaqRequestDto } from './update-faq.request.dto';
